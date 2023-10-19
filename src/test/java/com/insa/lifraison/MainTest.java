2e5bbfa683c2ddd941e8d227102809c30e9bab2d
swordfatih <accfldekur@gmail.com>
2023-10-19 09:48:59 +0200

    chore: init gradle project

---
+++ src/test/java/com/insa/lifraison/MainTest.java
@@ -0,0 +1,11 @@
+package com.insa.lifraison;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
