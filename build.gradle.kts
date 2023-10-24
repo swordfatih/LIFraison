cad3dea783dd3dea7ea49d454a395939b54ec423
swordfatih <accfldekur@gmail.com>
2023-10-24 10:25:59 +0200

    feat: init javafx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("com.github.jacobono.jaxb") version "1.3.5"
 }
 
 group = "com.insa.lifraison"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "com.insa.lifraison.Main"
+}
+
+javafx {
+    version = "12"
+    modules("javafx.controls", "javafx.fxml")
+}
 
 repositories {
     mavenCentral()
--- src/main/java/com/insa/lifraison/Main.java
+++ src/main/java/com/insa/lifraison/Main.java
@@ -1,22 +1,38 @@
 package com.insa.lifraison;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Objects;
+
 /**
  * Main class
  */
-public class Main {
+public class Main extends Application {
     /**
      * Entry point of the project
      * @param args No argument required
      */
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        launch(args);
+    }
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL scenePath = Objects.requireNonNull(getClass().getResource("scene.fxml"));
+        Parent root = FXMLLoader.load(scenePath);
+
+        Scene scene = new Scene(root);
+        URL stylePath = Objects.requireNonNull(getClass().getResource("styles.css"));
+        scene.getStylesheets().add(stylePath.toExternalForm());
+
+        stage.setTitle("L'IF'raison");
+        stage.setScene(scene);
+        stage.show();
     }
 }
