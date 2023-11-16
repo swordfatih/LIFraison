f9886c16ce762d4f69feb5f2c0315bb8925a70ed
Ambreeeeee <ambre.hutier@gmail.com>
2023-11-16 10:46:12 +0100

    comments of observable

--- src/main/java/com/insa/lifraison/observer/Selectable.java
+++ src/main/java/com/insa/lifraison/observer/Selectable.java
@@ -1,5 +1,10 @@
 package com.insa.lifraison.observer;
 
+/**
+ * Selectable is a class to know if an element is selected or not.
+ * If an element of the model is selected in one of the views,
+ * other views must also highlight it.
+ */
 public class Selectable {
     private boolean isSelected;
 
